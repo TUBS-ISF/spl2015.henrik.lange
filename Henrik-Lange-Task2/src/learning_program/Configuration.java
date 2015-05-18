4ac80c0497512c97cfc42aa58b5cecf1184bed34
hlange <henrik.lange@tu-bs.de>
2015-05-18 20:15:19 +0200

    runtime variables

--- Henrik-Lange-Task2/src/learning_program/Configuration.java
+++ Henrik-Lange-Task2/src/learning_program/Configuration.java
@@ -3,8 +3,8 @@ package learning_program;
 
 
 public class Configuration {
-	public static boolean oneVsM = true; // if false, than it will only compare
+	public static boolean oneVsM = false; // if false, than it will only compare
 											// the first entry
-	public static boolean topDown = true; // true for going through every entry, false random
+	public static boolean topDown = false; // true for going through every entry, false random
 
 }
--- Henrik-Lange-Task2/src/learning_program/Learning_Program.java
+++ Henrik-Lange-Task2/src/learning_program/Learning_Program.java
@@ -17,13 +17,26 @@ public class Learning_Program {
 		Scanner sc = new Scanner(System.in);
 		// tr.importData("Data/Training_Data.txt","default");
 		// tr.exportData("Data/Training_Data_export.txt");
+		for (String arg : args) {
+			if(arg.equals("-ovm")){
+				Configuration.oneVsM=true;
+				System.out.println("oneVsM activated");
+			}else if(arg.equals("-td")){
+				Configuration.topDown=true;
+				System.out.println("topDown activated");
+			}else if(arg.equals("-help")){
+				System.out.println("-ovm, oneVsM");
+				System.out.println("-td, topDown");
+				System.exit(0);
+			}
+		}
 		while (true) {
 			int option;
 			while (true) {
 				try {
 					System.out.println("Hauptmenü");
 					System.out.println("[1] Lernen");
-					System.out.println("[2] Ein1stellung");
+					System.out.println("[2] Einstellung");
 					System.out.println("[3] Beenden");
 					option = sc.nextInt();
 					break;
